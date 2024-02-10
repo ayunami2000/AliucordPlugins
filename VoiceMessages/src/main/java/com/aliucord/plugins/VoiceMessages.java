78cca70e74b1a38f0a7be63f4823b99bff188794
ayunami2000 <spwilliamsiam@gmail.com>
2024-02-10 13:35:52 -0500

    RAAAAHHHHHH

--- VoiceMessages/src/main/java/com/aliucord/plugins/VoiceMessages.java
+++ VoiceMessages/src/main/java/com/aliucord/plugins/VoiceMessages.java
@@ -250,12 +250,14 @@ public class VoiceMessages extends Plugin {
                     String durationStr = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
                     float seconds = (Integer.parseInt(durationStr) / 1000.0f);
 
+                    /*
                     if (replyref != null) {
                         Utils.showToast(replyref.a() + "");
                         Utils.showToast(replyref.b() + "");
                         Utils.showToast(replyref.c() + "");
                     }
-                    DiscordAPI.sendVoiceMessage(filename, seconds, waveform, discordid, replyref == null ? 0L : replyref.a(), replyref == null ? 0L : replyref.c(), replyref == null ? 0L : replyref.b(), extension);
+                    */
+                    DiscordAPI.sendVoiceMessage(filename, seconds, waveform, discordid, replyref == null ? 0L : replyref.b(), replyref == null ? 0L : replyref.c(), replyref == null ? 0L : replyref.a(), extension);
 
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                         mmr.close();
